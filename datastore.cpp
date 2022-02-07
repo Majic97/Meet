6508bbb61170e612fa36ebcb06997d4f2e669de6
Majic97 <vb.97@bk.ru>
2022-02-07 20:33:28 +0300

    First push, nothing is done

--- datastore.cpp
+++ datastore.cpp
@@ -4,3 +4,33 @@ DataStore::DataStore(QObject *parent) : QObject(parent)
 {
 
 }
+
+void DataStore::ReciveRequest(RequestStruc &request)
+{
+    mutex_request.lock();
+    RequestPool.append(request);
+    mutex_request.unlock();
+}
+
+bool DataStore::GetRequest(RequestStruc &req)
+{
+
+    mutex_answer.lock();
+
+    if(!AnswerPool.isEmpty())
+    {
+        mutex_request.lock();
+        req = RequestPool.first();
+        RequestPool.removeFirst();
+        mutex_request.unlock();
+        return true;
+    }else
+        return false;
+}
+
+void DataStore::SetAnswer(AnswerStruc &answ)
+{
+    mutex_answer.lock();
+    AnswerPool.append(answ);
+    mutex_answer.unlock();
+}
